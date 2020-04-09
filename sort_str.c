9208099cb7df8075a71d4e2aa744b1c95ae4f7c1
DribbleLife11 <55861791+DribbleLife11@users.noreply.github.com>
2020-04-09 23:58:14 +0800

    字符串排序
    字符串排序实例

---
+++ sort_str.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+#define SIZE 81
+#define LIM 20
+#define HALT " "
+void stsrt(char *strings[],int num);
+char * s_gets(char *st, int n);
+
+int main(void)
+{
+    char input[LIM][SIZE];
+    char *ptstr[LIM];
+    int ct = 0;
+    int k;
+
+    printf("Input up to %d lines,and I will sort them.\n",LIM);
+    printf("To stop, press the Enter key at a line's start.\n");
+    while (ct < LIM && s_gets(input[ct],SIZE) != NULL
+    && input[ct][0] != '\0')
+    {
+        ptstr[ct] = input[ct];
+        ct++;
+        /* code */
+    }
+    stsrt(ptstr, ct);
+    puts("\nHere's the sorted list: \n");
+    for(k = 0; k < ct; k++)
+        puts(ptstr[k]);
+
+    return 0;
+    
+}
+
+void stsrt(char *strings[], int num)
+{
+    char *temp;
+    int top,seek;
+
+    for(top = 0; top < num - 1; top++)
+        for(seek = top + 1; seek < num; seek++)
+            if(strcmp(strings[top],strings[seek])>0)
+            {
+                temp = strings[top];
+                strings[top] = strings[seek];
+                strings[seek] = temp;
+            }
+}
